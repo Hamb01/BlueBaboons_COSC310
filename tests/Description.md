b4fcd97092480a701ea8bdcde56f733b6c97f25a
Hamb01 <isaac.bobmeyer@gmail.com>
2026-09-25 10:30:27 -0600

    made some changes to the layout of the conftest file to clarify how/where new configurations for future tests should be added

--- tests/Description.md
+++
@@ -1,3 +0,0 @@
-Document to record changes/features in tests/
-
-We can delete this later if necessary, but git wouldn't allow me to push an empty folder
--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,13 @@ from app.api.deps import get_health_repository, get_health_service
 from app.repositories.health_repository import HealthRepository
 from app.services.health_service import HealthService
 
+# this document contains pytest fixtures,
+# fixtures are functions that can be injected into other tests 
+# they are just setup code so we don't need to redefine this stuff for every indivdual test
+
+############################################################################################
+### General Config for all tests
+############################################################################################
 
 @pytest.fixture
 def isolated_data_dir(tmp_path: Path) -> Path:
@@ -16,17 +23,7 @@ def isolated_data_dir(tmp_path: Path) -> Path:
     data_dir.mkdir(parents=True, exist_ok=True)
     return data_dir
 
-
-@pytest.fixture
-def isolated_health_repo(isolated_data_dir: Path) -> HealthRepository:
-    return HealthRepository(data_dir=isolated_data_dir)
-
-
-@pytest.fixture
-def isolated_health_service(isolated_health_repo: HealthRepository) -> HealthService:
-    return HealthService(repository=isolated_health_repo)
-
-
+# update this feature everytime an additional repo/service/anything must be tested
 @pytest.fixture
 def client(
     isolated_health_service: HealthService,
@@ -40,3 +37,24 @@ def client(
         yield test_client
 
     app.dependency_overrides.clear()
+
+
+############################################################################################
+### Config for test_health.py
+############################################################################################
+
+@pytest.fixture
+def isolated_health_repo(isolated_data_dir: Path) -> HealthRepository:
+    return HealthRepository(data_dir=isolated_data_dir)
+
+
+@pytest.fixture
+def isolated_health_service(isolated_health_repo: HealthRepository) -> HealthService:
+    return HealthService(repository=isolated_health_repo)
+
+
+############################################################################################
+### Config for restaurants.py
+############################################################################################
+
+
